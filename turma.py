88ea90682da7cb3add4f68e79270a6f7bf03925c
tonegi <90652377+tonegi@users.noreply.github.com>
2024-12-05 20:08:24 -0300

    Create turma.py

---
+++ turma.py
@@ -0,0 +1,18 @@
+class Turma:
+  def __init__(self):
+    self.turma = [];
+    self.menorNota = None;
+    self.maiorNota = None;
+
+  def cadastrarAlunos(self, alunos):    
+    for i in alunos:
+      self.turma.append(i);
+      if((self.menorNota == None) or (self.menorNota.nota > i.nota)):
+        self.menorNota = i;
+      elif((self.maiorNota == None) or (self.maiorNota.nota < i.nota)):
+        self.maiorNota = i;                 
+
+  def mostrarAlunos(self):  
+    print('Quantidade de alunos:', len(self.turma));
+    for x in self.turma:      
+      print(x.mostrarAluno());
